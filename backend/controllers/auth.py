b106b6fe41d672c03d16356aab6c339a6ff7ff7a
Abhiram B.S.N <abhirambsn@gmail.com>
2022-10-13 18:03:55 +0530

    Backend Code

--- backend/controllers/auth.py
+++ backend/controllers/auth.py
@@ -1,8 +1,11 @@
 import json
+import argon2
 from flask import Blueprint, jsonify, request
 from middleware.auth import token_required
 from models.customer import Customer
-from app import db
+from db import db
+from argon2 import PasswordHasher
+from sqlalchemy import select
 
 auth_bp = Blueprint('authentication_blueprint', __name__)
 
@@ -16,9 +19,23 @@ def login():
     body = request.json
     customer_id = body['customer_id']
     password = body['password']
-
+    customer_data_stmt = select(Customer).where(Customer.customer_id == customer_id)
+    customer_dataset = db.session.execute(customer_data_stmt)
+    customer_data = None
+    for cd in customer_dataset.scalars():
+        customer_data = cd
+    pwd_hash = customer_data.password
+    ph = PasswordHasher()
+    try:
+        ph.verify(pwd_hash, password)
+        return {'token': "ejwr", "msg":"Welcome "}
+    except argon2.exceptions.VerifyMismatchError as e:
+        return jsonify({'error': "Invalid Credentials"})
+    # print(customer_data.scalars()[0])
     return jsonify({'message': 'TODO: Implement LOGIN!'})
 
 @auth_bp.route('/register', methods=["POST"])
 def register():
+    data = request.json
+    print("Sent Data", data)
     return jsonify({'account': 'TODO: Implement REGISTER!!'})
